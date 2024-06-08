8d71ecb4223c5aa687f9760d83fb0c1c7ac232b3
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 16:55:48 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,45 @@
+  //store the location in a array. Make sure the array is not in alphabetical order.
+
+
+  let plases :string [] = ["Islamad","Pashwar","Multan","Karachi","Lahore"];
+
+ // print your array in its orignal orignal.
+
+ console.log("original order",plases);
+
+ // print your array in alphabetical order without modifying the actul list.
+
+ 
+ //show that your array is still in its original order by printing it .
+
+ console.log("original order:",plases);
+
+ //print your array in revers alphabetical order by printing it.
+
+
+ console.log("original order:",plases);
+
+ //reverse the order of your list.point the array to show that is order has changed.
+
+ console.log("reverse order:",plases.reverse());
+
+//reverse the order of your list.print the array to show that its order has changed.
+
+console.log("orignal order:",plases.reverse());
+
+//sort your aarray so its stored in reverse alphabetical order.print the 
+  //array to show that its order has been chanched.
+
+  console.log("Alphabetical order:", [plases]. sort());
+
+  //sort to change your array so its stored in reverse alphabetical order.print the list to show that its 
+ // order has changed.
+
+ console.log(" Reverse Alphabetical order:",[ plases]. sort(). reverse());
+
+
+
+
+
+
+
